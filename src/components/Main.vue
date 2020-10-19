c4384ff7aa204b53f44f856a994f1b8b6fead266
JuanDuran85 <duran85juan@gmail.com>
2020-10-19 20:31:16 -0300

    nuevos cambios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="mt-5">
       <JomboTron/>
-      <Cards class="mt-5"/>
+      <Cards class="mt-5" :dataCards="dataVarios"/>
   </main>
 </template>
 
@@ -14,7 +14,20 @@ export default {
     components: {
         JomboTron,
         Cards
-    }
+    },
+    data() {
+        return {
+           /*  url: 'https://picsum.photos/id/237/200/300'  */  
+           dataVarios: [
+                {urlImg: 'https://picsum.photos/id/237/200/300', id: '1', texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', enlace: 'https://es.wikipedia.org/wiki/Portal:Perros'},
+                {urlImg: 'https://picsum.photos/seed/picsum/200/300', id: '2', texto: 'Illo commodi modi ullam mollitia sapiente provident debitis consequatur quasi nesciunt voluptatibus.', enlace: 'https://es.wikipedia.org/wiki/Monta%C3%B1a'},
+                {urlImg: 'https://picsum.photos/id/237/200/300', id: '3', texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', enlace: 'https://es.wikipedia.org/wiki/Portal:Perros'},
+                {urlImg: 'https://picsum.photos/seed/picsum/200/300', id: '4', texto: 'Illo commodi modi ullam mollitia sapiente provident debitis consequatur quasi nesciunt voluptatibus.', enlace: 'https://es.wikipedia.org/wiki/Monta%C3%B1a'},
+                {urlImg: 'https://picsum.photos/id/237/200/300', id: '5', texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', enlace: 'https://es.wikipedia.org/wiki/Portal:Perros'},
+                {urlImg: 'https://picsum.photos/seed/picsum/200/300', id: '6', texto: 'Illo commodi modi ullam mollitia sapiente provident debitis consequatur quasi nesciunt voluptatibus.', enlace: 'https://es.wikipedia.org/wiki/Monta%C3%B1a'}
+           ]   
+        }
+    },
 }
 </script>
 
--- src/components/mainComponents/Cards.vue
+++ src/components/mainComponents/Cards.vue
@@ -3,7 +3,7 @@
       <div class="row">
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -13,7 +13,7 @@
           </div>
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -23,7 +23,7 @@
           </div>
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,7 +33,7 @@
           </div>
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -43,7 +43,7 @@
           </div>
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -53,7 +53,7 @@
           </div>
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
             <div class="card">
-                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <img :src="urlImg" class="card-img-top" alt="img1">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -67,7 +67,18 @@
 
 <script>
 export default {
-    name: 'Cards'
+    name: 'Cards',
+    props: {
+/*         urlImg: {
+            type: String,
+            required: true,
+            default: "https://picsum.photos/seed/picsum/200/300"
+        }, */
+        dataCards: {
+            type: Array,
+            required: true
+        }
+    }
 }
 </script>
 
