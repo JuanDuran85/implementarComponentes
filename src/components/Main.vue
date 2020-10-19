49beb02ff7f061994d575fccb2d5b9fe17604e2d
JuanDuran85 <duran85juan@gmail.com>
2020-10-19 20:09:23 -0300

    trabajando con componentes

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="mt-5">
+      <JomboTron/>
+      <Cards class="mt-5"/>
+  </main>
+</template>
+
+<script>
+import JomboTron from './mainComponents/JumboTron.vue';
+import Cards from './mainComponents/Cards.vue';
+
+export default {
+    name: 'Main',
+    components: {
+        JomboTron,
+        Cards
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/mainComponents/Cards.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="card">
+                <img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="img1">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Cards'
+}
+</script>
+
+<style>
+
+</style>
